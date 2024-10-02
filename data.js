e73e3b976cc8bb969d7b302050176159056026d3
sgbi <83344965+sgbi@users.noreply.github.com>
2024-10-02 21:21:47 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,487 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_0015",
+      "name": "DJI_0015",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.067406002752243,
+        "pitch": 0.9276336369511675,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.33469552278604,
+          "pitch": 0.2225063117602133,
+          "rotation": 0,
+          "target": "1-dji_0017"
+        },
+        {
+          "yaw": 2.9268843204935715,
+          "pitch": 0.15254429737226793,
+          "rotation": 0,
+          "target": "2-dji_0018"
+        },
+        {
+          "yaw": -2.888064371420061,
+          "pitch": 0.12535890441977848,
+          "rotation": 0,
+          "target": "3-dji_0019"
+        },
+        {
+          "yaw": -2.437096327414496,
+          "pitch": 0.2648881242482446,
+          "rotation": 0,
+          "target": "4-dji_0020"
+        },
+        {
+          "yaw": -1.893300705016328,
+          "pitch": 0.5470140881618804,
+          "rotation": 0,
+          "target": "5-dji_0021"
+        },
+        {
+          "yaw": -1.1741853079170994,
+          "pitch": -0.015545291631680058,
+          "rotation": 0,
+          "target": "6-dji_0022"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dji_0017",
+      "name": "DJI_0017",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.35577674964509143,
+        "pitch": 0.612238200387754,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0869445596645573,
+          "pitch": 0.7474474483269624,
+          "rotation": 0,
+          "target": "2-dji_0018"
+        },
+        {
+          "yaw": 2.776147738450966,
+          "pitch": 0.3735844734878633,
+          "rotation": 0,
+          "target": "3-dji_0019"
+        },
+        {
+          "yaw": -2.7860285600852386,
+          "pitch": 0.4075703958595227,
+          "rotation": 0,
+          "target": "4-dji_0020"
+        },
+        {
+          "yaw": -2.284643383576583,
+          "pitch": 0.40412511237144244,
+          "rotation": 0,
+          "target": "5-dji_0021"
+        },
+        {
+          "yaw": -1.905424040963604,
+          "pitch": 0.44889985902572427,
+          "rotation": 0,
+          "target": "0-dji_0015"
+        },
+        {
+          "yaw": -2.2875396795296368,
+          "pitch": 0.11267890595753549,
+          "rotation": 0,
+          "target": "6-dji_0022"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dji_0018",
+      "name": "DJI_0018",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.27656755839295855,
+        "pitch": 0.8365818256382589,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6559972596864725,
+          "pitch": 0.28089234513204,
+          "rotation": 0,
+          "target": "0-dji_0015"
+        },
+        {
+          "yaw": -2.630666178428598,
+          "pitch": 0.5079147771389607,
+          "rotation": 0,
+          "target": "1-dji_0017"
+        },
+        {
+          "yaw": 1.0244716215544223,
+          "pitch": 0.42014838059724546,
+          "rotation": 0,
+          "target": "3-dji_0019"
+        },
+        {
+          "yaw": 1.8911305159938445,
+          "pitch": 0.35803652283059506,
+          "rotation": 0,
+          "target": "4-dji_0020"
+        },
+        {
+          "yaw": 2.3764675169533556,
+          "pitch": 0.283722719864258,
+          "rotation": 0,
+          "target": "5-dji_0021"
+        },
+        {
+          "yaw": 2.225581117595235,
+          "pitch": 0.039917214993389294,
+          "rotation": 0,
+          "target": "6-dji_0022"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dji_0019",
+      "name": "DJI_0019",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.4994326782630072,
+        "pitch": 0.5769881221835966,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.120999091212596,
+          "pitch": 0.31565545999187705,
+          "rotation": 0,
+          "target": "1-dji_0017"
+        },
+        {
+          "yaw": -1.473050632125645,
+          "pitch": 0.5378745831794358,
+          "rotation": 0,
+          "target": "2-dji_0018"
+        },
+        {
+          "yaw": -2.904561672503105,
+          "pitch": 0.26717554652737263,
+          "rotation": 0,
+          "target": "0-dji_0015"
+        },
+        {
+          "yaw": 2.754627598859037,
+          "pitch": 0.46371744104905765,
+          "rotation": 0,
+          "target": "4-dji_0020"
+        },
+        {
+          "yaw": 3.029240185191023,
+          "pitch": 0.3110692943132687,
+          "rotation": 0,
+          "target": "5-dji_0021"
+        },
+        {
+          "yaw": 2.6454578977985586,
+          "pitch": 0.06326657541990599,
+          "rotation": 0,
+          "target": "6-dji_0022"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-dji_0020",
+      "name": "DJI_0020",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.11546024282424483,
+        "pitch": 0.6122309343849945,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7641969399083095,
+          "pitch": 0.5124260863972214,
+          "rotation": 0,
+          "target": "0-dji_0015"
+        },
+        {
+          "yaw": -2.3294438806026427,
+          "pitch": 0.3253663894426122,
+          "rotation": 0,
+          "target": "1-dji_0017"
+        },
+        {
+          "yaw": -1.5718962141010415,
+          "pitch": 0.3666388168213288,
+          "rotation": 0,
+          "target": "2-dji_0018"
+        },
+        {
+          "yaw": -0.8494870654369109,
+          "pitch": 0.4356097769480449,
+          "rotation": 0,
+          "target": "3-dji_0019"
+        },
+        {
+          "yaw": 2.2582843137122417,
+          "pitch": 0.8164191655060282,
+          "rotation": 0,
+          "target": "5-dji_0021"
+        },
+        {
+          "yaw": 1.6548875372794472,
+          "pitch": 0.18026625222690384,
+          "rotation": 0,
+          "target": "6-dji_0022"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dji_0021",
+      "name": "DJI_0021",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.0187958534830166,
+        "pitch": 0.5176195694187413,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4976424166646378,
+          "pitch": 0.5488370536663307,
+          "rotation": 0,
+          "target": "0-dji_0015"
+        },
+        {
+          "yaw": 2.7044331814932754,
+          "pitch": 0.23892853512080414,
+          "rotation": 0,
+          "target": "1-dji_0017"
+        },
+        {
+          "yaw": -2.915658332777875,
+          "pitch": 0.21396199222086665,
+          "rotation": 0,
+          "target": "2-dji_0018"
+        },
+        {
+          "yaw": -2.334268971777391,
+          "pitch": 0.1831104834691626,
+          "rotation": 0,
+          "target": "3-dji_0019"
+        },
+        {
+          "yaw": -1.5938179862150683,
+          "pitch": 0.5439491780000605,
+          "rotation": 0,
+          "target": "4-dji_0020"
+        },
+        {
+          "yaw": 0.019256440166836697,
+          "pitch": 0.06044859693295379,
+          "rotation": 0,
+          "target": "6-dji_0022"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-dji_0022",
+      "name": "DJI_0022",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.12620073052881686,
+        "pitch": 0.8812494875137666,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3588549202793647,
+          "pitch": 0.5436118758285495,
+          "rotation": 0,
+          "target": "0-dji_0015"
+        },
+        {
+          "yaw": -0.17933530142520837,
+          "pitch": 0.34245250063415433,
+          "rotation": 0,
+          "target": "1-dji_0017"
+        },
+        {
+          "yaw": 0.15800404856945605,
+          "pitch": 0.3365707066730508,
+          "rotation": 0,
+          "target": "2-dji_0018"
+        },
+        {
+          "yaw": 0.48281460434068,
+          "pitch": 0.3551306977213926,
+          "rotation": 0,
+          "target": "3-dji_0019"
+        },
+        {
+          "yaw": 0.3633739167449015,
+          "pitch": 0.5643987018497896,
+          "rotation": 0,
+          "target": "4-dji_0020"
+        },
+        {
+          "yaw": -0.005268678542115879,
+          "pitch": 0.5687384547181509,
+          "rotation": 0,
+          "target": "5-dji_0021"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Capitol Building Roof",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
